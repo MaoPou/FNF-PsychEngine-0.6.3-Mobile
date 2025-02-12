e35795f93fa8b174ca43bf908aee042138270998
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-02-12 11:57:58 +0800

    Update Hitbox.hx

--- source/mobile/Hitbox.hx
+++ source/mobile/Hitbox.hx
@@ -83,20 +83,20 @@ class Hitbox extends MobileInputManager implements IMobileControls
 				add(buttonUp = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 4), FlxG.height, 0xFF12FA05));
 				add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), 0, Std.int(FlxG.width / 4), FlxG.height, 0xFFF9393F));
 			case SINGLE:
-				add(buttonLeft = createHint(0, offsetSec, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFFC24B99));
-				add(buttonDown = createHint(FlxG.width / 4, offsetSec, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFF00FFFF));
-				add(buttonUp = createHint(FlxG.width / 2, offsetSec, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFF12FA05));
-				add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), offsetSec, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3,
-					0xFFF9393F));
-				add(buttonExtra = createHint(0, offsetFir, FlxG.width, Std.int(FlxG.height / 4), 0xFF0066FF));
+				add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFFC24B99));
+		                add(buttonDown = createHint(Std.int(FlxG.width / 5), 0, Std.int(FlxG.width / 5), FlxG.height, 0xFF00FFFF));
+				add(buttonExtra = createHint(Std.int(FlxG.width / 5) * 2, 0, Std.int(FlxG.width / 5);
+				add(buttonUp = createHint(Std.int(FlxG.width / 5) * 3, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFF12FA05));
+	                        add(buttonRight = createHint(Std.int(FlxG.width / 5) * 4, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFFF9393F));
+				
 			case DOUBLE:
-				add(buttonLeft = createHint(0, offsetSec, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFFC24B99));
-				add(buttonDown = createHint(FlxG.width / 4, offsetSec, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFF00FFFF));
-				add(buttonUp = createHint(FlxG.width / 2, offsetSec, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFF12FA05));
-				add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), offsetSec, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3,
-					0xFFF9393F));
-				add(buttonExtra2 = createHint(Std.int(FlxG.width / 2), offsetFir, Std.int(FlxG.width / 2), Std.int(FlxG.height / 4), 0xA6FF00));
-				add(buttonExtra = createHint(0, offsetFir, Std.int(FlxG.width / 2), Std.int(FlxG.height / 4), 0xFF0066FF));
+				add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFFC24B99));
+		                add(buttonDown = createHint(Std.int(FlxG.width / 5), 0, Std.int(FlxG.width / 5), FlxG.height, 0xFF00FFFF));
+				add(buttonExtra = createHint(Std.int(FlxG.width / 5) * 2, 0, Std.int(FlxG.width / 5), Std.int(FlxG.height / 2), 0xFFFF00));
+                                add(buttonExtra2 = createHint(Std.int(FlxG.width / 5) * 2, Std.int(FlxG.height / 2), Std.int(FlxG.width / 5), Std.int(FlxG.height / 2), 0xFFFF00));
+				add(buttonUp = createHint(Std.int(FlxG.width / 5) * 3, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFF12FA05));
+	                        add(buttonRight = createHint(Std.int(FlxG.width / 5) * 4, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFFF9393F));
+				
 		}
 
 		for (button in Reflect.fields(this))
