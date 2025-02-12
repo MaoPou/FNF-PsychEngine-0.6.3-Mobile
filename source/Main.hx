28e2d7fec6d8933eb2abaefee7f19261616c7c56
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-02-12 12:07:09 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -28,8 +28,6 @@ extern "C" HRESULT WINAPI SetCurrentProcessExplicitAppUserModelID(PCWSTR AppID);
 ')
 #end
 
-using StringTools;
-
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
